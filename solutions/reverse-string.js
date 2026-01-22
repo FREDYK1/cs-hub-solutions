dd04ec7286ad6c12d15203a990a8dcb45c8eae5d
Frederick Kwaku Kankam <fkkankam@st.ug.edu.gh>
2026-01-22 22:52:25 +0000

    Add solution for: Reverse String

---
+++ solutions/reverse-string.js
@@ -0,0 +1,16 @@
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        left, right = 0 , len(s) - 1
+
+        while left < right:
+            r = s[left]
+            s[left] = s[right]
+            s[right] = r
+            left +=1
+            right -=1
+
+        return s
